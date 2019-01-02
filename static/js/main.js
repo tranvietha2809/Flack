f7b898f39026043ec72efb7b3b84d0f3e9ff4e4a
Ha Tran <tranvietha.2809@gmail.com>
2019-01-02 15:30:35 +0200

    Revamp design pattern

--- static/js/main.js
+++ static/js/main.js
@@ -1,13 +1,20 @@
 
-  var socket = io.connect(location.protocol + '//' + document.domain + ':' + location.port);
-  //var socket = io()
-  var username;
-  var channel = "Global";
-  var message;
+var socket = io.connect(location.protocol + '//' + document.domain + ':' + location.port);
+
+$(document).ready(function(){
+  // check if user is already registered
+  if (localStorage.getItem('username') != null){
+    $("h3").html('');
+    $("h3").append('<div>'+ localStorage.getItem('username') + "</div>");
+  }
+
+  //default channel is Global channel
+  localStorage.setItem("default_channel", "Global");
+//
   $("#submit_username").on("submit", function(){
-    username = $(".username").val().trim();
+    let username = $(".username").val().trim();
     socket.emit("username input", {
-      "username":username
+      "username": username
     })
     return false;
   })
@@ -18,31 +25,10 @@
       $("h3").append("<div>Username is already taken</div>")
     }
     else {
+      localStorage.setItem('username', data.username);
       $("h3").html('');
-      $("h3").append('<div>'+ data.username + "</div>");
+      $("h3").append('<div>'+ localStorage.getItem('username') + "</div>");
     }
-  })
-
-  $("#user_message").on("submit", function(){
-    message = $("#message_field").val().trim();
-    let message_time = new Date();
-    let time_stamp = message_time.getTime();
-    if(message !== ""){
-      socket.emit("send message", {
-        "channel" : channel,
-        "username": username,
-        "message" : message,
-        "time_stamp": time_stamp
-      })
-    }
-    $("#message_field").val('').focus();
-    return false;
-  })
-
-  socket.on("received message", function(data){
-    $(".msg_history").append(
-      '<div class="outgoing_msg"><div class="sent_msg"><p>' +data.username + '</p> <p> '+ data.message +'</p></div></div>'
-    )
   })
 
   $("#create_channel").on("submit", function(){
@@ -56,14 +42,52 @@
     return false;
   })
 
-  socket.on("channel created", function(data){
+  socket.on("created channel", function(data){
     $(".inbox_chat").append(
+      '<a href = "#" class = "channel"><div class="chat_list active_chat"><div class="chat_people"><div class="chat_ib"><h5 id ="' +data.channel_name+ '">'+ data.channel_name+'</h5></div></div></div></a>')
+  })
+
+  $(".channel").on("click", function(){
+    localStorage.setItem("current_channel", $(this).find("h5").text())
+
+    //Clear message display
+    $(".msg_history").empty();
+
+    //load message of that channel up
+  })
+})
+  /**$("#user_message").on("submit", function(){
+    "message = $("#message_field").val().trim();
+    let message_time = new Date();
+    let time_stamp = message_time.getTime();
+    if(message !== ""){
+      "socket.emit("send message", {
+        "channel" : channel,
+        "username": username,
+        "message" : message,
+        "time_stamp": time_stamp
+      })
+    }
+    "$("#message_field").val('').focus();
+    return false;
+  })
+
+  socket.on("received message", function(data){
+    "$(".msg_history").append(
+      '<div class="outgoing_msg"><div class="sent_msg"><p>' +data.username + '</p> <p> '+ data.message +'</p></div></div>'
+    )
+  })
+
+
+
+  socket.on("channel created", function(data){
+    ""$(".inbox_chat").append(
       '<a href = "/'+data.channel_name+ '" class = "channel"><div class="chat_list active_chat"><div class="chat_people"><div class="chat_ib"><h5 id ="' +data.channel_name+ '">'+ data.channel_name+'</h5></div></div></div></a>'
     )
   })
 
   $("body").on("click", '.channel', function(e){
-    e.preventDefault();
+    "e.preventDefault();
     channel = '';
     channel = $(".channel").find("h5").text();
     socket.emit("get channel messages", {
@@ -73,6 +97,6 @@
   })
 
   socket.on("receive channel messages", function(data){
-    let user_messages, others_messages;
+    "let user_messages, others_messages;
     console.log(data)
-  })
+  })**/
